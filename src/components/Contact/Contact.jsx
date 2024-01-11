0dd9082a5ea8fa9186b4f1f143765975f9c2d294
bk1071 <bk1071@students.cs.ubca>
2024-01-10 16:51:37 -0800

    mobile version changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,23 +8,23 @@ export const Contact = () => {
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <p>Let's get in touch!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">bilal.khan1071@hotmail.com</a>
+          <a href="mailto:myemail@email.com" target="_blank">bilal.khan1071@hotmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/bilalkhan1071/">linkedin.com/bilalkhan1071</a>
+          <a href="https://www.linkedin.com/in/bilalkhan1071/" target="_blank">linkedin.com/bilalkhan1071</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/Bilalkhan1071">github.com/bilalkhan1071</a>
+          <a href="https://github.com/Bilalkhan1071" target="_blank">github.com/bilalkhan1071</a>
         </li>
       </ul>
     </footer>
